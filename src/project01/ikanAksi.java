9926f4bad3d52bd7d4b9b225e86862ccf236921e
irwanputrasetia <irwanputrastya@gmail.com>
2018-03-26 17:07:29 +0700

    Mengisi Nilai Attribut

--- src/project01/ikanAksi.java
+++ src/project01/ikanAksi.java
@@ -12,5 +12,22 @@ package project01;
 public class ikanAksi {
     public static void main(String[] args) {
         Ikan lumba_lumba = new Ikan();
+        Ikan paus = new Ikan();
+        Ikan hiu = new Ikan();
+        
+        lumba_lumba.alatnapas="Paru-Paru";
+        lumba_lumba.gigi="Tajam Kecil";
+        lumba_lumba.makan="Ikan Kecil atau Cumi-cumi";
+        lumba_lumba.produksi="Melahirkan";
+        
+        paus.alatnapas="Paru-Paru";
+        paus.gigi="Graham Serabut";
+        paus.makan="Plankton atau ikan kecil";
+        paus.produksi="Melahirkan";
+        
+        hiu.alatnapas="Insang";
+        hiu.gigi="Taring Tajam";
+        hiu.makan="Semua Daging";
+        hiu.produksi="Bertelur";
     }
 }
