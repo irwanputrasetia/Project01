fae2a73e37e206c1ba94aab7abcd738a0b57ebbe
irwanputrasetia <irwanputrastya@gmail.com>
2018-03-26 16:54:38 +0700

    Membuat Atribut dan Behavior

--- src/project01/Ikan.java
+++ src/project01/Ikan.java
@@ -10,5 +10,16 @@ package project01;
  * @author irwan
  */
 public class Ikan {
+    String alatnapas;
+    String produksi;
+    String gigi;
+    String makan;
     
+    void cetakInfo(){
+        System.out.println("Alatnapas :"+alatnapas);
+        System.out.println("Produksi :" +produksi);
+        System.out.println("Gigi :"+gigi);
+        System.out.println("Makanan :"+makan);
+        
+    }
 }
